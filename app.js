eddb008bcd8775dd9430cfbf510077ccd101685c
Dustin Lawless <djlaw90@gmail.com>
2019-11-13 08:49:31 -0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+
+/*
+Slideshow Styles
+Help from:
+https://www.sitepoint.com/make-a-simple-javascript-slideshow-without-jquery/
+*/
+
+var slides = document.querySelectorAll('#slides .slide');
+var currentSlide = 0;
+var slideInterval = setInterval(nextSlide,5000);
+
+function nextSlide(){
+    slides[currentSlide].className = 'slide';
+    currentSlide = (currentSlide+1)%slides.length;
+    slides[currentSlide].className = 'slide showing';
+}
